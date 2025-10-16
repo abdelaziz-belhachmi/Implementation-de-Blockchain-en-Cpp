043b3d4e077ab84abf492f9b0941efd2b98cab91
abdelaziz-belhachmi <abdelaziz.belhachmi@etu.uae.ac.ma>
2025-10-16 19:13:15 +0100

    PROOF_OF_STAKE

--- 3- Proof of Stake/proof_of_stake.cpp
+++ 3- Proof of Stake/proof_of_stake.cpp
@@ -2,7 +2,73 @@
 // Created by abdelaziz on 10/16/2025.
 //
 
-#ifndef IMPLEMENTATION_DE_BLOCKCHAIN_EN_CPP_PROOF_OF_STAKE_H
-#define IMPLEMENTATION_DE_BLOCKCHAIN_EN_CPP_PROOF_OF_STAKE_H
+#include "proof_of_stake.h"
+#include "proof_of_work.h"
+#include <iostream>
+#include <chrono>
 
-#endif //IMPLEMENTATION_DE_BLOCKCHAIN_EN_CPP_PROOF_OF_STAKE_H
+int main() {
+    std::cout << "=== Test Proof of Stake ===" << std::endl << std::endl;
+
+    BlockchainPoS blockchainPoS;
+
+    blockchainPoS.addValidator("Validator_A", 100);
+    blockchainPoS.addValidator("Validator_B", 200);
+    blockchainPoS.addValidator("Validator_C", 50);
+    blockchainPoS.addValidator("Validator_D", 150);
+
+    std::cout << "Validateurs ajoutes:" << std::endl;
+    for(int i = 0; i < blockchainPoS.getPoS().getValidatorCount(); i++) {
+        Validator v = blockchainPoS.getPoS().getValidator(i);
+        std::cout << "  " << v.address << " - Stake: " << v.stake << std::endl;
+    }
+    std::cout << std::endl;
+
+    auto startPoS = std::chrono::high_resolution_clock::now();
+
+    for(int i = 1; i <= 5; i++) {
+        std::stringstream ss;
+        ss << "Block " << i << " Data PoS";
+        BlockPoS block(i, blockchainPoS.getLastBlock().getHash(), ss.str());
+        blockchainPoS.addBlock(block);
+
+        std::cout << "Block " << i << " valide par: "
+                  << blockchainPoS.getBlock(i).getValidator() << std::endl;
+        std::cout << "Hash: " << blockchainPoS.getBlock(i).getHash() << std::endl;
+    }
+
+    auto endPoS = std::chrono::high_resolution_clock::now();
+    auto durationPoS = std::chrono::duration_cast<std::chrono::microseconds>(endPoS - startPoS);
+
+    std::cout << std::endl << "Temps d'execution PoS: " << durationPoS.count() << " microseconds" << std::endl;
+    std::cout << "Chaine PoS valide: " << (blockchainPoS.isChainValid() ? "OUI" : "NON") << std::endl;
+
+    std::cout << std::endl << "=== Comparaison PoW vs PoS ===" << std::endl << std::endl;
+
+    int difficulty = 3;
+    std::cout << "Test avec difficulte PoW = " << difficulty << std::endl << std::endl;
+
+    Blockchain blockchainPoW;
+
+    auto startPoW = std::chrono::high_resolution_clock::now();
+
+    for(int i = 1; i <= 5; i++) {
+        std::stringstream ss;
+        ss << "Block " << i << " Data PoW";
+        Block block(i, blockchainPoW.getLastBlock().getHash(), ss.str());
+        block.mineBlock(difficulty);
+        blockchainPoW.addBlock(block);
+    }
+
+    auto endPoW = std::chrono::high_resolution_clock::now();
+    auto durationPoW = std::chrono::duration_cast<std::chrono::microseconds>(endPoW - startPoW);
+
+    std::cout << "Temps d'execution PoW: " << durationPoW.count() << " microseconds" << std::endl;
+    std::cout << "Temps d'execution PoS: " << durationPoS.count() << " microseconds" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Ratio PoW/PoS: " << (double)durationPoW.count() / durationPoS.count() << "x" << std::endl;
+    std::cout << "Methode la plus rapide: " << (durationPoS.count() < durationPoW.count() ? "PoS" : "PoW") << std::endl;
+
+    return 0;
+}
--- 3- Proof of Stake/proof_of_stake.h
+++ 3- Proof of Stake/proof_of_stake.h
@@ -5,4 +5,163 @@
 #ifndef IMPLEMENTATION_DE_BLOCKCHAIN_EN_CPP_PROOF_OF_STAKE_H
 #define IMPLEMENTATION_DE_BLOCKCHAIN_EN_CPP_PROOF_OF_STAKE_H
 
+#include <string>
+#include <vector>
+#include <map>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+#include <openssl/sha.h>
+#include <random>
+
+class Validator {
+public:
+    std::string address;
+    int stake;
+
+    Validator(const std::string& addr, int stakeAmount)
+        : address(addr), stake(stakeAmount) {}
+};
+
+class BlockPoS {
+private:
+    int index;
+    std::string previousHash;
+    std::string data;
+    time_t timestamp;
+    std::string hash;
+    std::string validatorAddress;
+
+    std::string calculateHash(const std::string& input) {
+        unsigned char hash[SHA256_DIGEST_LENGTH];
+        SHA256((unsigned char*)input.c_str(), input.size(), hash);
+
+        std::stringstream ss;
+        for(int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+            ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+        }
+        return ss.str();
+    }
+
+public:
+    BlockPoS(int idx, const std::string& prevHash, const std::string& blockData)
+        : index(idx), previousHash(prevHash), data(blockData), validatorAddress("") {
+        timestamp = time(nullptr);
+        hash = "";
+    }
+
+    void validateBlock(const std::string& validator) {
+        validatorAddress = validator;
+        hash = calculateBlockHash();
+    }
+
+    std::string calculateBlockHash() {
+        std::stringstream ss;
+        ss << index << previousHash << data << timestamp << validatorAddress;
+        return calculateHash(ss.str());
+    }
+
+    std::string getHash() const { return hash; }
+    int getIndex() const { return index; }
+    std::string getPreviousHash() const { return previousHash; }
+    std::string getData() const { return data; }
+    time_t getTimestamp() const { return timestamp; }
+    std::string getValidator() const { return validatorAddress; }
+};
+
+class ProofOfStake {
+private:
+    std::vector<Validator> validators;
+    std::mt19937 rng;
+
+public:
+    ProofOfStake() : rng(std::random_device{}()) {}
+
+    void addValidator(const std::string& address, int stake) {
+        validators.push_back(Validator(address, stake));
+    }
+
+    std::string selectValidator() {
+        if(validators.empty()) {
+            return "";
+        }
+
+        int totalStake = 0;
+        for(const auto& v : validators) {
+            totalStake += v.stake;
+        }
+
+        std::uniform_int_distribution<int> dist(0, totalStake - 1);
+        int randomValue = dist(rng);
+
+        int cumulativeStake = 0;
+        for(const auto& v : validators) {
+            cumulativeStake += v.stake;
+            if(randomValue < cumulativeStake) {
+                return v.address;
+            }
+        }
+
+        return validators[0].address;
+    }
+
+    int getValidatorCount() const { return validators.size(); }
+
+    Validator getValidator(int index) const { return validators[index]; }
+};
+
+class BlockchainPoS {
+private:
+    std::vector<BlockPoS> chain;
+    ProofOfStake pos;
+
+public:
+    BlockchainPoS() {
+        chain.push_back(createGenesisBlock());
+    }
+
+    BlockPoS createGenesisBlock() {
+        BlockPoS genesis(0, "0", "Genesis Block PoS");
+        genesis.validateBlock("Genesis");
+        return genesis;
+    }
+
+    BlockPoS getLastBlock() {
+        return chain.back();
+    }
+
+    void addValidator(const std::string& address, int stake) {
+        pos.addValidator(address, stake);
+    }
+
+    void addBlock(BlockPoS newBlock) {
+        std::string validator = pos.selectValidator();
+        newBlock.validateBlock(validator);
+        chain.push_back(newBlock);
+    }
+
+    bool isChainValid() {
+        for(size_t i = 1; i < chain.size(); i++) {
+            BlockPoS currentBlock = chain[i];
+            BlockPoS previousBlock = chain[i - 1];
+
+            if(currentBlock.getHash() != currentBlock.calculateBlockHash()) {
+                return false;
+            }
+
+            if(currentBlock.getPreviousHash() != previousBlock.getHash()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    size_t getSize() const { return chain.size(); }
+
+    BlockPoS getBlock(int index) const { return chain[index]; }
+
+    ProofOfStake& getPoS() { return pos; }
+};
+
+
 #endif //IMPLEMENTATION_DE_BLOCKCHAIN_EN_CPP_PROOF_OF_STAKE_H
