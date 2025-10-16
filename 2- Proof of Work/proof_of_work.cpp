fd17994a9ef9f80bb8bd5fdcd2b2993dc17efd09
abdelaziz-belhachmi <abdelaziz.belhachmi@etu.uae.ac.ma>
2025-10-16 19:11:22 +0100

    proof_of_work

--- 2- Proof of Work/proof_of_work.cpp
+++ 2- Proof of Work/proof_of_work.cpp
@@ -2,7 +2,52 @@
 // Created by abdelaziz on 10/16/2025.
 //
 
-#ifndef IMPLEMENTATION_DE_BLOCKCHAIN_EN_CPP_PROOF_OF_WORK_H
-#define IMPLEMENTATION_DE_BLOCKCHAIN_EN_CPP_PROOF_OF_WORK_H
+#include "proof_of_work.h"
+#include <iostream>
+#include <chrono>
 
-#endif //IMPLEMENTATION_DE_BLOCKCHAIN_EN_CPP_PROOF_OF_WORK_H
+int main() {
+    std::cout << "=== Test Proof of Work avec differentes difficultes ===" << std::endl << std::endl;
+
+    for(int difficulty = 1; difficulty <= 5; difficulty++) {
+        std::cout << "Difficulte: " << difficulty << std::endl;
+
+        Blockchain blockchain;
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+        Block block1(1, blockchain.getLastBlock().getHash(), "Block 1 Data");
+        block1.mineBlock(difficulty);
+        blockchain.addBlock(block1);
+
+        Block block2(2, blockchain.getLastBlock().getHash(), "Block 2 Data");
+        block2.mineBlock(difficulty);
+        blockchain.addBlock(block2);
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+        std::cout << "Block 1 Hash: " << block1.getHash() << std::endl;
+        std::cout << "Block 1 Nonce: " << block1.getNonce() << std::endl;
+        std::cout << "Block 2 Hash: " << block2.getHash() << std::endl;
+        std::cout << "Block 2 Nonce: " << block2.getNonce() << std::endl;
+        std::cout << "Temps d'execution: " << duration.count() << " ms" << std::endl;
+        std::cout << "Chaine valide: " << (blockchain.isChainValid() ? "OUI" : "NON") << std::endl;
+        std::cout << std::endl;
+    }
+
+    std::cout << "=== Test de verification d'integrite ===" << std::endl;
+    Blockchain testChain;
+
+    Block b1(1, testChain.getLastBlock().getHash(), "Transaction 1");
+    b1.mineBlock(3);
+    testChain.addBlock(b1);
+
+    Block b2(2, testChain.getLastBlock().getHash(), "Transaction 2");
+    b2.mineBlock(3);
+    testChain.addBlock(b2);
+
+    std::cout << "Chaine initiale valide: " << (testChain.isChainValid() ? "OUI" : "NON") << std::endl;
+
+    return 0;
+}
--- 2- Proof of Work/proof_of_work.h
+++ 2- Proof of Work/proof_of_work.h
@@ -5,4 +5,106 @@
 #ifndef IMPLEMENTATION_DE_BLOCKCHAIN_EN_CPP_PROOF_OF_WORK_H
 #define IMPLEMENTATION_DE_BLOCKCHAIN_EN_CPP_PROOF_OF_WORK_H
 
+#include <string>
+#include <vector>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+#include <openssl/sha.h>
+
+class Block {
+private:
+    int index;
+    std::string previousHash;
+    std::string data;
+    time_t timestamp;
+    int nonce;
+    std::string hash;
+
+    std::string calculateHash(const std::string& input) {
+        unsigned char hash[SHA256_DIGEST_LENGTH];
+        SHA256((unsigned char*)input.c_str(), input.size(), hash);
+
+        std::stringstream ss;
+        for(int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+            ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+        }
+        return ss.str();
+    }
+
+public:
+    Block(int idx, const std::string& prevHash, const std::string& blockData)
+        : index(idx), previousHash(prevHash), data(blockData), nonce(0) {
+        timestamp = time(nullptr);
+        hash = "";
+    }
+
+    void mineBlock(int difficulty) {
+        std::string target(difficulty, '0');
+
+        do {
+            nonce++;
+            hash = calculateBlockHash();
+        } while (hash.substr(0, difficulty) != target);
+    }
+
+    std::string calculateBlockHash() {
+        std::stringstream ss;
+        ss << index << previousHash << data << timestamp << nonce;
+        return calculateHash(ss.str());
+    }
+
+    std::string getHash() const { return hash; }
+    int getIndex() const { return index; }
+    std::string getPreviousHash() const { return previousHash; }
+    std::string getData() const { return data; }
+    time_t getTimestamp() const { return timestamp; }
+    int getNonce() const { return nonce; }
+};
+
+class Blockchain {
+private:
+    std::vector<Block> chain;
+
+public:
+    Blockchain() {
+        chain.push_back(createGenesisBlock());
+    }
+
+    Block createGenesisBlock() {
+        Block genesis(0, "0", "Genesis Block");
+        genesis.mineBlock(1);
+        return genesis;
+    }
+
+    Block getLastBlock() {
+        return chain.back();
+    }
+
+    void addBlock(Block newBlock) {
+        chain.push_back(newBlock);
+    }
+
+    bool isChainValid() {
+        for(size_t i = 1; i < chain.size(); i++) {
+            Block currentBlock = chain[i];
+            Block previousBlock = chain[i - 1];
+
+            if(currentBlock.getHash() != currentBlock.calculateBlockHash()) {
+                return false;
+            }
+
+            if(currentBlock.getPreviousHash() != previousBlock.getHash()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    size_t getSize() const { return chain.size(); }
+
+    Block getBlock(int index) const { return chain[index]; }
+};
+
+
 #endif //IMPLEMENTATION_DE_BLOCKCHAIN_EN_CPP_PROOF_OF_WORK_H
